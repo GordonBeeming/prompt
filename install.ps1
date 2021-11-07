792014009991cf0d22dedd75f16898cb3dd9f246
CI System <gordonbeeming@outlook.com>
2021-11-07 20:30:16 +0200

    checkpoint

---
+++ install.ps1
@@ -0,0 +1,12 @@
+winget install JanDeDobbeleer.OhMyPosh
+winget install github.cli
+
+# install fonts
+# set font in vs code
+# set font in terminal
+
+Install-Module -Name Terminal-Icons -Repository PSGallery
+
+Install-Module PSReadLine -RequiredVersion 2.2.0-beta1 -AllowPrerelease
+
+install-module z -AllowClobber
