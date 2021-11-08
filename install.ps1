05b69d8cf2c5871086d124aa5e5332c6d13f689d
Gordon Beeming 🤼⚙🛠 <Gordon.Beeming@derivco.co.za>
2021-11-08 05:26:14 +0200

    minor updates

--- install.ps1
+++ install.ps1
@@ -2,12 +2,12 @@ winget install JanDeDobbeleer.OhMyPosh
 winget install github.cli
 
 # install fonts
-# set font in vs code
+# set font in vs code (should sync)
 # set font in terminal
 
 Install-Module -Name Terminal-Icons -Repository PSGallery
 
-Install-Module PSReadLine -RequiredVersion 2.2.0-beta1 -AllowPrerelease
+Install-Module PSReadLine -RequiredVersion 2.2.0-beta4 -AllowPrerelease
 
 install-module z -AllowClobber
 
